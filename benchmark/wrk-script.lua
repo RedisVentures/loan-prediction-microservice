f92ca2992bbaf0570f33e7e18f227d49a673ceb4
Sam Partee <sam.partee@redis.com>
2023-01-12 20:11:22 +0000

    add benchmark tool

---
+++ benchmark/wrk-script.lua
@@ -0,0 +1,4 @@
+
+wrk.method = "POST"
+wrk.body = '{"zipcode":"12542", "dob_ssn":"19510613_1349", "age": 29, "income": 100000, "emp_length": 3, "loan_amount": 10000000, "int_rate": 7.2 }'
+wrk.headers["Content-Type"] = "application/json"
